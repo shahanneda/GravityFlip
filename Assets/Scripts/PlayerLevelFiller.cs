cd0e79b20b88c55a98e49282e9ea9f365ad34a18
Shahan Neda <shahan.neda@gmail.com>
2020-01-18 22:25:47 -0800

    added new level types and refactored level generation

--- Assets/Scripts/PlayerLevelFiller.cs
+++ Assets/Scripts/PlayerLevelFiller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerLevelFiller : MonoBehaviour
 {
 
-    public GameObject levelPrefabs;
+    public GameObject[] levelPrefabs;
     public Transform levelParent;
     public int levelPrebuffNumber = 10;
     private GameObject currentGenerationLevel = null;
@@ -13,10 +13,12 @@ public class PlayerLevelFiller : MonoBehaviour
 
     public void Start()
     {
-        GameObject newLevel = Instantiate(levelPrefabs, transform.position, Quaternion.identity, levelParent);
+        GameObject newLevel = Instantiate(levelPrefabs[0], transform.position, Quaternion.identity, levelParent);
         currentGenerationLevel = newLevel;
-        for (int i = 0; i < levelPrebuffNumber; i++){
-            GenerateNewLevel();
+
+        for (int i = 0; i < levelPrebuffNumber; i++)
+        {
+            GenerateRandomNewLevel();
         }
     }
 
@@ -29,19 +31,26 @@ public class PlayerLevelFiller : MonoBehaviour
     public void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.name == "Level End"){
-            GenerateNewLevel();
+            GenerateRandomNewLevel();
         } 
     }
+    private GameObject PickRandomNewLevel(){
+        return levelPrefabs[Random.Range(0,levelPrefabs.Length)];
+    }
 
-    private void GenerateNewLevel(){
-
-        Vector3 newLevelPos = getCurrentLevelEnd().transform.position;
-        Vector3 levelStartLocalPos = levelPrefabs.transform.Find("Level Start").localPosition;
-        newLevelPos = newLevelPos - levelStartLocalPos;
-
-        GameObject newLevel = Instantiate(levelPrefabs, newLevelPos, Quaternion.identity, levelParent);
+    private void CreateNewLevel(GameObject levelPrefab){
+        GameObject newLevel = Instantiate(levelPrefab, GetNewLevelPosition(levelPrefab), Quaternion.identity, levelParent);
         currentGenerationLevel = newLevel;
-          
+    }
+    private Vector3 GetNewLevelPosition(GameObject level){
+        Vector3 newLevelPos = getCurrentLevelEnd().transform.position;
+        Vector3 levelStartLocalPos = level.transform.Find("Level Start").localPosition;
+        newLevelPos = newLevelPos - levelStartLocalPos;
+        return newLevelPos;
+    }
+
+    public void GenerateRandomNewLevel(){
+        CreateNewLevel(PickRandomNewLevel());
     }
 }
 
